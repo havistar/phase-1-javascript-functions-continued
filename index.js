d8a57b4a752cb95d8adab32be35e0b4881f3d80d
Christina Nguyen <cnguyen4290@yahoo.com>
2021-08-12 10:27:57 -0500

    commit tree 7e025c095589cf53b785d01675760f231a77a3eb

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code here
+function saturdayFun(){
+    const activity = "roller-skate"
+    console.log(`This Saturday, I want to ${activity}!`)
+    console.log("This Saturday, I want to bathe my dog.")
+}
+saturdayFun();
